13781629736d57b0682f439cbbf3934d6cecc5fd
lilasorann <carrentals32ew@gmail.com>
2025-06-08 11:20:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ SESSION_ID: process.env.SESSION_ID || "",
 // add your Session Id make sure it starts with malvin~
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "𝓥𝓲𝓬𝓽𝓸𝓻",
+BOT_NAME: process.env.BOT_NAME || "VENOM",
 // add bot name here for menu
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
@@ -56,7 +56,7 @@ LINK_WHITELIST: "youtube.com,github.com",
 // add alive msg here 
 
 
-        STICKER_NAME: process.env.STICKER_NAME || "𝓥𝓲𝓬𝓽𝓸𝓻",
+        STICKER_NAME: process.env.STICKER_NAME || "VENOM",
 // type sticker pack name 
 
         CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
@@ -68,10 +68,10 @@ LINK_WHITELIST: "youtube.com,github.com",
           DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
 
-          OWNER_NUMBER: process.env.OWNER_NUMBER || "2347050510935",
+          OWNER_NUMBER: process.env.OWNER_NUMBER || "2349162835238",
 // add your bot owner number
 
-OWNER_NAME: process.env.OWNER_NAME || "𝓥𝓲𝓬𝓽𝓸𝓻",
+OWNER_NAME: process.env.OWNER_NAME || "VENOM",
 // add bot owner name
 
               DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝓥𝓲𝓬𝓽𝓸𝓻*",
